b409c3f57a5e1c2fd2d5dfb9f2b1fceca763f3c6
Josh McKee <josh@snazzware.com>
2017-05-20 23:17:41 -0500

    First commit

---
+++ spec/token-select-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import TokenSelectView from '../lib/token-select-view';
+
+describe('TokenSelectView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
